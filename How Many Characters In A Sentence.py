b7c1bfda3d0e12510b957af6711282ec2b7cfdfe
Tamrazrk <135056938+Tamrazrk@users.noreply.github.com>
2023-07-26 10:48:21 +0400

    Create How Many Characters In A Sentence.py

---
+++ How Many Characters In A Sentence.py
@@ -0,0 +1,6 @@
+my_text = "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
+
+# Find the number of characters in my_text
+num_characters = len(my_text)
+
+print("Number of characters in the text:", num_characters)
